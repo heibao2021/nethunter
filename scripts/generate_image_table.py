edd8bacf833c301b3a908b84de8e0f2dcd703a43
Re4son <3520622-re4son@users.noreply.gitlab.com>
2020-04-27 11:38:36 +1000

    Finish migration to yaml for meta data
    Signed-off-by: Re4son <3520622-re4son@users.noreply.gitlab.com>

--- scripts/generate_image_table.py
+++ scripts/generate_image_table.py
@@ -3,6 +3,8 @@
 import yaml # install pyyaml
 OUTPUT_FILE = './images.md'
 INPUT_FILE = './devices.cfg'
+qty_devices = 0
+qty_images = 0
 
 ## Input:
 ## ------------------------------------------------------------ ##
@@ -32,18 +34,23 @@ def parse(content):
     lines = data.split('\n')
     for line in lines:
         if line.startswith('##*'):
-            result += line[3:] + "\n"
+            ## yaml doesn't like tabs so let's replace them with four spaces 
+            result += line.replace('\t', '    ')[3:] + "\n"
     return yaml.load(result, Loader=yaml.FullLoader)
 
 def generate_device_table(data):
+    global qty_devices
+    global qty_images
     default = ""
     table = "| Device | Display Name | Kernel ID | Android Version | Status | Notes |\n|---|---|---|---|---|---|\n"
     # iterate over all the devices
     for element in data:
         # iterate over all the versions
         for key in element.keys():
+            qty_devices += 1
             if 'images' in element[key]:
                 for image in element[key]['images']:
+                    qty_images += 1
                     table += "| {} | {} | {} | {} | {} | {} |\n".format(key, image.get('name', default), image.get('id', default), image.get('os', default), image.get('status', default), image.get('note', default))
     return table
 
@@ -51,7 +58,10 @@ res = parse(data)
 generated_markdown = generate_device_table(res)
 
 with open(OUTPUT_FILE, 'w') as f:
+    quantities = "The NetHunter repository contains kernels for *" + str(qty_devices) + "* devices.  \n"
+    quantities += "The next release cycle will include *" + str(qty_images) + "* NetHunter images.  \n\n" 
     f.write(str(header))
+    f.write(str(quantities))
     f.write(str(generated_markdown))
     f.close()
 
