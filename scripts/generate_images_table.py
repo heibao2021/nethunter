6a5b74344704cb75512766f63f7b62db23f44924
Ben Wilson <g0tmi1k@kali.org>
2022-06-20 05:43:25 +0100

    Consistency over scripts

--- scripts/generate_images_table.py
+++ scripts/generate_images_table.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-import yaml # python3 -m pip install pyyaml --user
 from datetime import datetime
+import sys
+import yaml # python3 -m pip install pyyaml --user
 
 OUTPUT_FILE = './images.md'
 INPUT_FILE = './devices.cfg'
@@ -29,21 +30,21 @@ qty_images = 0
 ##*         doco   : ""
 ##*         note:    "** Warning: Android ten is very unstable at the moment. **"
 
-def parse(data):
+def yaml_parse(content):
     result = ""
-    lines = data.split('\n')
+    lines = content.split('\n')
     for line in lines:
         if line.startswith('##*'):
             ## yaml doesn't like tabs so let's replace them with four spaces
             result += line.replace('\t', '    ')[3:] + "\n"
-    return yaml.load(result, Loader=yaml.FullLoader)
+    return yaml.safe_load(result)
 
-def generate_device_table(data):
-    global qty_devices
-    global qty_images
+def generate_table(data):
+    global qty_devices, qty_images
     default = ""
     table  = "| Display Name (Android OS) | Device | Kernel ID | Android Version | Rootfs | Status | Documentation Link | Notes |\n"
     table += "|---------------------------|--------|-----------|-----------------|--------|--------|--------------------|-------|\n"
+
     # iterate over all the devices
     for element in data:
         # iterate over all the versions
@@ -52,41 +53,69 @@ def generate_device_table(data):
             if 'images' in element[key]:
                 for image in element[key]['images']:
                     qty_images += 1
+                    doco=image.get('doco', default)
+                    if doco:
+                        doco = "<" + doco + ">"
                     table += "| {} | {} | {} | {} | {} | {} | {} | {} |\n".format(image.get('name', default),
                                                                                   key,
                                                                                   image.get('id', default),
                                                                                   image.get('os', default),
                                                                                   image.get('rootfs', default),
                                                                                   image.get('status', default),
-                                                                                  image.get('doco', default),
+                                                                                  doco,
                                                                                   image.get('note', default))
     return table
 
-def get_versions():
-    with open(INPUT_FILE) as f:
-        data = f.read()
-        f.close()
-        return parse(data)
+def read_file(file):
+    try:
+        with open(file) as f:
+            data = f.read()
+            f.close()
+    except Exception as e:
+        print("[-] Cannot open input file: {} - {}".format(file, e))
+    return data
 
-def write_markdown():
-    with open(OUTPUT_FILE, 'w') as f:
-        meta  = '---\n'
-        meta += 'title: Official Kali NetHunter Images\n'
-        meta += '---\n\n'
-        stats  = "- The Kali NetHunter repository contains kernels for [**{}** devices](nethunter-imagestats.html)\n".format(str(qty_devices))
-        stats += "- The next release cycle will include **{}** [Kali NetHunter images](https://www.offensive-security.com/kali-linux-nethunter-download/)\n\n".format(str(qty_images))
-        f.write(str(meta))
-        f.write(str(stats))
-        f.write(str(generated_markdown))
-        f.write(str(repo_msg))
-        f.close()
+def write_file(data, file):
+    global repo_msg, qty_devices, qty_images
+    try:
+        with open(file, 'w') as f:
+            meta  = '---\n'
+            meta += 'title: Kali NetHunter Images\n'
+            meta += '---\n\n'
+            stats  = "- The [next release](https://www.kali.org/releases/) cycle will include **{}** Kali NetHunter images _([ready to download](https://www.kali.org/get-kali/#kali-mobile))_\n".format(str(qty_images))
+            stats += "- The official [Kali NetHunter repository](https://gitlab.com/kalilinux/nethunter/build-scripts/kali-nethunter-devices) contains [kernels](kernels.html) for [**{}** devices](image-stats.html) _([here](https://gitlab.com/kalilinux/nethunter/build-scripts/kali-nethunter-devices/-/blob/master/devices.cfg))_\n".format(str(qty_devices))
+            stats += "- [Kali NetHunter Statistics](index.html)\n\n"
+            f.write(str(meta))
+            f.write(str(stats))
+            f.write(str(data))
+            f.write(str(repo_msg))
+            f.close()
+            print('[+] File: {} successfully written'.format(OUTPUT_FILE))
+    except Exception as e:
+        print("[-] Cannot write to output file: {} - {}".format(file, e))
+    return 0
 
-def print_text():
-    print('File: {} successfully written'.format(OUTPUT_FILE))
+def print_summary():
+    global qty_devices, qty_images
     print('Devices: {}'.format(qty_devices))
     print('Images : {}'.format(qty_images))
 
-res = get_versions()
-generated_markdown = generate_device_table(res)
-write_markdown()
-print_text()
+def main(argv):
+    # Assign variables
+    data = read_file(INPUT_FILE)
+
+    # Get data
+    res = yaml_parse(data)
+    generated_markdown = generate_table(res)
+
+    # Create markdown file
+    write_file(generated_markdown, OUTPUT_FILE)
+
+    # Print result
+    print_summary()
+
+    # Exit
+    exit(0)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- scripts/generate_kernels_table.py
+++ scripts/generate_kernels_table.py
@@ -1,30 +1,61 @@
 #!/usr/bin/env python3
-import yaml # python3 -m pip install pyyaml --user
 from datetime import datetime
+import sys
+import yaml # python3 -m pip install pyyaml --user
 
 OUTPUT_FILE = './kernels.md'
 INPUT_FILE = './kernels.yml'
 repo_msg = "\n_This table was generated automatically on {} from the [Kali NetHunter GitLab repository](https://gitlab.com/kalilinux/nethunter/build-scripts/kali-nethunter-devices)_\n".format(datetime.now().strftime("%Y-%B-%d %H:%M:%S"))
-kernels_number = 0
+qty_models = 0
+qty_kernels = 0
 
-def sanitize_content(data):
+## Input:
+## ------------------------------------------------------------ ##
+##- a5uelte:
+##    model   : Samsung Galaxy A5 (2015)
+##    kernels :
+##      - id          : a5ulte
+##        description : Galaxy A5 (2015) for CyanogenMod
+##        versions    :
+##          - android      : marshmallow
+##            linux        : '3.10'
+##            kernel       : 1.3
+##            description  : CyanogenMod 13
+##            author       : DeadSquirrel01
+##            source       : 'git clone https://github.com/DeadSquirrel01/nethunter-kernel-a5ulte.git -b cm-13.0'
+##            features     : []
+##      - id          : a5ulte-touchwiz
+##        description : Galaxy A5 (2015) for TouchWiz (Europe)
+##        versions    :
+##          - android      : marshmallow
+##            linux        : '3.10'
+##            kernel       : 1.3
+##            description  : TouchWiz 6
+##            author       : DeadSquirrel01
+##            source       : 'git clone https://github.com/DeadSquirrel01/nethunter-kernel-a5ulte.git -b touchwiz-6.0'
+##            features     : []
+
+def yaml_parse(data):
     result = ""
     lines = data.split('\n')
     for line in lines:
         if len(line) > 0 and line[0] != '#':
             result += line + '\n'
-    return result
+    return yaml.safe_load(result)
 
-def generate_device_table(data):
-    global kernels_number
+def generate_table(data):
+    global qty_kernels, qty_models
     default = ""
     table  = "| Display Name | Kernel ID | Android Version | Linux Version | Kernel Version | Description | Features | Author | Source |\n"
     table += "|--------------|-----------|-----------------|---------------|----------------|-------------|----------|--------|--------|\n"
+
     for element in data:
         for kernel_name in element.keys():
             model = element[kernel_name]['model']
+            qty_models += 1
             for kernel in element[kernel_name]['kernels']:
                 for version in kernel['versions']:
+                    qty_kernels += 1
                     features = ""
                     i = 0
                     for f in version.get('features', default):
@@ -38,36 +69,62 @@ def generate_device_table(data):
                                                                                          version.get('linux', default),
                                                                                          version.get('kernel', default),
                                                                                          version.get('description', default),
-                                                                                         features, version.get('author', default),
+                                                                                         features,
+                                                                                         version.get('author', default),
                                                                                          version.get('source', default))
-    kernels_number = len(table.split('\n'))-3
+    #qty_kernels = len(table.split('\n'))-3
     return table
 
-def get_versions():
-    with open(INPUT_FILE) as f:
-        data = f.read()
-        content = sanitize_content(data)
-        return yaml.load(content, Loader=yaml.FullLoader)
+def read_file(file):
+    try:
+        with open(file) as f:
+            data = f.read()
+            f.close()
+    except Exception as e:
+        print("[-] Cannot open input file: {} - {}".format(file, e))
+    return data
 
-def write_markdown():
-    global kernels_number
-    with open(OUTPUT_FILE, 'w') as g:
-        meta  = '---\n'
-        meta += 'title: Official Kali NetHunter Kernels\n'
-        meta += '---\n\n'
-        stats = "- The Kali NetHunter repository contains [**{}** kernels](nethunter-kernelstats.html)\n\n".format(str(kernels_number))
-        g.write(str(meta))
-        g.write(str(stats))
-        g.write(generated_markdown)
-        g.write(str(repo_msg))
-        g.close()
+def write_file(data, file):
+    global repo_msg,qty_kernels
+    try:
+        with open(file, 'w') as f:
+            meta  = '---\n'
+            meta += 'title: Kali NetHunter Kernels\n'
+            meta += '---\n\n'
+            stats  = "- The official [Kali NetHunter repository](https://gitlab.com/kalilinux/nethunter/build-scripts/kali-nethunter-devices) is using [**{}** kernels](kernel-stats.html) _([here](https://gitlab.com/kalilinux/nethunter/build-scripts/kali-nethunter-devices/-/blob/master/kernels.yml))_\n".format(str(qty_kernels))
+            stats += "- These kernels can be used on **{}** models\n".format(str(qty_models))
+            stats += "- [Kali NetHunter Statistics](index.html)\n\n"
+            f.write(str(meta))
+            f.write(str(stats))
+            f.write(str(data))
+            f.write(str(repo_msg))
+            f.close()
+            print('[+] File: {} successfully written'.format(OUTPUT_FILE))
+    except Exception as e:
+        print("[-] Cannot write to output file: {} - {}".format(file, e))
+    return 0
 
-def print_text():
-    global kernels_number
-    print('File: {} successfully written'.format(OUTPUT_FILE))
-    print('Kernels: {}'.format(kernels_number))
+def print_summary():
+    global qty_kernels, qty_models
+    print('Kernels: {}'.format(qty_kernels))
+    print('Models : {}'.format(qty_models))
 
-res = get_versions()
-generated_markdown = generate_device_table(res)
-write_markdown()
-print_text()
+def main(argv):
+    # Assign variables
+    data = read_file(INPUT_FILE)
+
+    # Get data
+    res = yaml_parse(data)
+    generated_markdown = generate_table(res)
+
+    # Create markdown file
+    write_file(generated_markdown, OUTPUT_FILE)
+
+    # Print result
+    print_summary()
+
+    # Exit
+    exit(0)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
